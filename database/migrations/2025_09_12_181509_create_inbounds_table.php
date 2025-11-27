00908c00d28867617bb3af95ba92f6c34e94b0b7
Hamed <hamed.mmc@gmail.com>
2025-11-28 01:36:02 +0330

    Edit Readme file

--- database/migrations/2025_09_12_181509_create_inbounds_table.php
+++ database/migrations/2025_09_12_181509_create_inbounds_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
         Schema::create('inbounds', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->json('inbound_data');
             $table->integer('inbound_id')->nullable();
             $table->timestamps();
         });
